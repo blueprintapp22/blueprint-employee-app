e4e4d484a99968eb45a7a641fbfac2c1299f9023
Nick Lopez <nicklopez5454@yahoo.com>
2022-08-25 09:01:26 -0500

    server work

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+const logger = require('morgan')
+const AuthRouter = require('./routes/AuthRouter')
+// const AppRouter = require('./routes/AppRouter')
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.use('/auth', AuthRouter)
+// app.use('/app', AppRouter)
+app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
