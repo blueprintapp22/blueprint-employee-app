305445fd8ce962105d0287a85fdfd92a771390b9
Frank Marra <frankrmarra@gmail.com>
2022-11-29 23:15:04 -0500

    working on acquiring token

--- controllers/QuickbooksController.js
+++ controllers/QuickbooksController.js
@@ -1,16 +1,26 @@
 const { compareSync } = require('bcrypt')
 const Quickbooks = require('node-quickbooks')
+const OAuthClient = require('intuit-oauth')
+const axios = require('axios')
 
-let clientId = 'ABPkh6TLJ6WrbWPGa1aoWfLP3NusHNwgP33bX8q26yVnBT50Pc'
-let clientSecret = 'MrLZSS6qFawcQdFahSIE6a5aGoPfjYWYHe62Cztl'
-let oauthToken =
-  'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0.._dxrq9ggU-bldW3MA4D8Eg.dh18s93SCFsxc0HKfj4ZtP7kpNK-dWkrjNgzdQQsHiflvyD2Gop6iHi53_FLOqYnz7Qa-ym6OcJCmAJa3bxA3E3Zhj6Ew41T7A0e5DOybpHA-NR-GAfi8bWmFHBL9wKuZ65QuLnRmGELh5cEvjCZIU5FcaL26UzJ98orjC7vu3fvEsfmE-MZvRt1DT8mG15cvSFo8jiJ2qZzIWHn-9B3LpyFNP0o0HQk5GDtnG1b8YdrI7x8i5VYDA7Jqm-CRf66Xs5ybtCnOw4dsyaATc8WHQlkLxBjVPzj1pQqLmlL5cPWvHKHBbOdMcxbwNQyepUc_LPiROHm347ZSY8VfENJkKjCh1W_ggeura-kUGhO0nkpqZ7D1YHcNgA6MgA_wSDr4T9sWLkRkEBGrlwAddvG24klct7oIbNjZ0y0PxDPoEs3Hb-WtEphpCYsFLSYIgOi3HZSZA2-G4OhkNN4YERJoKROTRxLyiySUjqhvqu377BsTPbWJvdrW7o2H4JyaB1m9zthRN1NlGCo_eY9ZbSzNnXnKMAmSvZEU6senylEtd0MOZcMezEfX_XWi9nOZTJDJQffLSQkA63IOECM7LFU-b4tVutqooyaS_hu3IIk59QbLG-vXVbhIsR_2LqFqg4GKGNG-SWAHeTATSIinhXtCo2q_T75I_nU8w9WpmfwVPqo0j4DMyCzPYpOKaoVWHtjVTjfWjpKNhNdLtqGygF62pZJtmuIO4AzdO3wonsuwPw.7LG8Uob8RkFx2msU2HI_DA'
+// let clientId = 'ABPkh6TLJ6WrbWPGa1aoWfLP3NusHNwgP33bX8q26yVnBT50Pc'
+// let clientSecret = 'MrLZSS6qFawcQdFahSIE6a5aGoPfjYWYHe62Cztl'
+
 let realmId = '1342692165'
-let refreshToken = 'AB11674321952ID5GHoqAkCXeX9U45LLUbutCJwJr2ZacfzfwG'
+let oauthToken = ''
+let refreshToken = ''
+
+const oauthClient = new OAuthClient({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  environment: 'production',
+  redirectUri: 'https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl',
+  logging: true
+})
 
 let qbo = new Quickbooks(
-  clientId,
-  clientSecret,
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
   oauthToken,
   false,
   realmId,
@@ -20,6 +30,25 @@ let qbo = new Quickbooks(
   '2.0',
   refreshToken
 )
+
+const GetToken = async (req, res) => {
+  try {
+    const res = await axios.post(
+      `https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer?grant_type=authorization_code&code=${process.env.INTUIT_AUTH_CODE}&redirect_uri=${OAuthClient.redirectUri}`,
+      {
+        headers: {
+          Authorization:
+            'Basic QUJQa2g2VExKNldyYldQR2ExYW9XZkxQM051c0hOd2dQMzNiWDhxMjZ5Vm5CVDUwUGM6TXJMWlNTNnFGYXdjUWRGYWhTSUU2YTVhR29QZmpZV1lIZTYyQ3p0bA'
+        }
+      }
+    )
+
+    console.log(res)
+    return res
+  } catch (error) {
+    throw error
+  }
+}
 const BusinessGetter = (req, res) => {
   try {
     let { id } = req.params
@@ -54,5 +83,6 @@ const InvoiceChecker = (req, res) => {
 }
 module.exports = {
   BusinessGetter,
-  InvoiceChecker
+  InvoiceChecker,
+  GetToken
 }
