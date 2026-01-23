c061c51cc8343a594167b07ce08f03e37ff1a070
nicklope <nicklopez5454@yahoo.com>
2026-01-23 10:07:34 -0600

    Remove quickbooks oauth temp

--- controllers/QuickbooksController.js
+++ controllers/QuickbooksController.js
@@ -1,131 +1,130 @@
-const { compareSync } = require('bcrypt')
-const Quickbooks = require('node-quickbooks')
-const OAuthClient = require('intuit-oauth')
+// const { compareSync } = require('bcrypt')
+// const Quickbooks = require('node-quickbooks')
+// const OAuthClient = require('intuit-oauth')
 
-let oauthToken = null
+// let oauthToken = null
 
-let qbo = null
+// let qbo = null
 
-let oauthClient = new OAuthClient({
-  clientId: process.env.CLIENT_ID,
+// let oauthClient = new OAuthClient({
+//   clientId: process.env.CLIENT_ID,
+//   clientSecret: process.env.CLIENT_SECRET,
 
-  clientSecret: process.env.CLIENT_SECRET,
+//   environment: 'production',
 
-  environment: 'production',
+//   redirectUri:
+//     'https://blueprint-employee-app-production.up.railway.app/bea/quickbooks/callback',
 
-  redirectUri:
-    'https://blueprint-employee-app-production.up.railway.app/bea/quickbooks/callback',
+//   logging: true
+// })
 
-  logging: true
-})
+// const GetToken = (req, res) => {
+//   try {
+//     const authUri = oauthClient.authorizeUri({
+//       scope: [OAuthClient.scopes.Accounting],
+//       state: 'intuit-test'
+//     })
 
-const GetToken = (req, res) => {
-  try {
-    const authUri = oauthClient.authorizeUri({
-      scope: [OAuthClient.scopes.Accounting],
-      state: 'intuit-test'
-    })
+//     res.redirect(authUri)
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-    res.redirect(authUri)
-  } catch (error) {
-    throw error
-  }
-}
+// const Callback = async (req, res) => {
+//   try {
+//     await oauthClient.createToken(req.url).then(function (authResponse) {
+//       oauthToken = JSON.stringify(authResponse.getJson(), null, 2)
+//     })
 
-const Callback = async (req, res) => {
-  try {
-    await oauthClient.createToken(req.url).then(function (authResponse) {
-      oauthToken = JSON.stringify(authResponse.getJson(), null, 2)
-    })
+//     qbo = new Quickbooks(
+//       process.env.CLIENT_ID,
+//       process.env.CLIENT_SECRET,
+//       oauthClient.getToken().access_token,
+//       false,
+//       oauthClient.getToken().realmId,
+//       false,
+//       true,
+//       null,
+//       '2.0',
+//       oauthClient.getToken().refresh_token
+//     )
 
-    qbo = new Quickbooks(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      oauthClient.getToken().access_token,
-      false,
-      oauthClient.getToken().realmId,
-      false,
-      true,
-      null,
-      '2.0',
-      oauthClient.getToken().refresh_token
-    )
+//     res.redirect(
+//       process.env.NODE_ENV === 'production'
+//         ? 'https://bpbd.info'
+//         : 'http://localhost:3000'
+//     )
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-    res.redirect(
-      process.env.NODE_ENV === 'production'
-        ? 'https://bpbd.io'
-        : 'http://localhost:3000'
-    )
-  } catch (error) {
-    throw error
-  }
-}
+// const RefreshAccessToken = (req, res) => {
+//   try {
+//     oauthClient.refresh().then(function (authResponse) {
+//       oauthToken = JSON.stringify(authResponse.getJson(), null, 2)
+//     })
+//     qbo = new Quickbooks(
+//       process.env.CLIENT_ID,
 
-const RefreshAccessToken = (req, res) => {
-  try {
-    oauthClient.refresh().then(function (authResponse) {
-      oauthToken = JSON.stringify(authResponse.getJson(), null, 2)
-    })
-    qbo = new Quickbooks(
-      process.env.CLIENT_ID,
+//       process.env.CLIENT_SECRET,
 
-      process.env.CLIENT_SECRET,
+//       oauthClient.getToken().access_token,
+//       false,
+//       oauthClient.getToken().realmId,
+//       false,
+//       true,
+//       null,
+//       '2.0',
+//       oauthClient.getToken().refresh_token
+//     )
 
-      oauthClient.getToken().access_token,
-      false,
-      oauthClient.getToken().realmId,
-      false,
-      true,
-      null,
-      '2.0',
-      oauthClient.getToken().refresh_token
-    )
+//     res.send(oauthToken.data)
+//   } catch (error) {
+//     throw error
+//   }
+// }
+// const BusinessGetter = (req, res) => {
+//   try {
+//     let { id } = req.params
 
-    res.send(oauthToken.data)
-  } catch (error) {
-    throw error
-  }
-}
-const BusinessGetter = (req, res) => {
-  try {
-    let { id } = req.params
+//     qbo.findInvoices({ DocNumber: id }, (err, invoice) => {
+//       if (err) console.log(err)
+//       if (!invoice.QueryResponse.Invoice) {
+//         res.send("No invoice's found")
+//       } else {
+//         res.send(invoice.QueryResponse.Invoice[0].CustomerRef.value)
+//       }
+//     })
+//   } catch (error) {
+//     res.send(error)
+//   }
+// }
+// const InvoiceChecker = (req, res) => {
+//   try {
+//     const { id } = req.params
+//     qbo.findInvoices(
+//       [
+//         {
+//           field: 'CustomerRef',
+//           value: id
+//         }
+//       ],
+//       (err, invoice) => {
+//         if (err) console.log(err)
 
-    qbo.findInvoices({ DocNumber: id }, (err, invoice) => {
-      if (err) console.log(err)
-      if (!invoice.QueryResponse.Invoice) {
-        res.send("No invoice's found")
-      } else {
-        res.send(invoice.QueryResponse.Invoice[0].CustomerRef.value)
-      }
-    })
-  } catch (error) {
-    res.send(error)
-  }
-}
-const InvoiceChecker = (req, res) => {
-  try {
-    const { id } = req.params
-    qbo.findInvoices(
-      [
-        {
-          field: 'CustomerRef',
-          value: id
-        }
-      ],
-      (err, invoice) => {
-        if (err) console.log(err)
-
-        res.send(invoice.QueryResponse.Invoice)
-      }
-    )
-  } catch (error) {
-    res.send(error)
-  }
-}
-module.exports = {
-  BusinessGetter,
-  InvoiceChecker,
-  GetToken,
-  Callback,
-  RefreshAccessToken
-}
+//         res.send(invoice.QueryResponse.Invoice)
+//       }
+//     )
+//   } catch (error) {
+//     res.send(error)
+//   }
+// }
+// module.exports = {
+//   BusinessGetter,
+//   InvoiceChecker,
+//   GetToken,
+//   Callback,
+//   RefreshAccessToken
+// }
